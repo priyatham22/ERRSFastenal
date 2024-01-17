aa8bb5add11ee8b25431c464b1b95d2975ac5faf
raswanth <raswanthvijai@gmail.com>
2024-01-17 21:43:50 +0530

    adjust db fields and leaderboard

--- app.py
+++ app.py
@@ -1,72 +1,38 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://{user}:{password}@{server}/{database}'.format(user='root', password='mayank14', server='127.0.0.1:5000', database='mysqldb')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:password@localhost/temp_ERP'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
 class User(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20), unique=True, nullable=False)
-    email = db.Column(db.String(120), unique=True, nullable=False)
-    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    __tablename__ = 'Users'
+    user_id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    username = db.Column(db.String(50), unique=True, nullable=False)
     password = db.Column(db.String(60), nullable=False)
-    posts = db.relationship('Post', backref='emp', lazy=True)
-
-    def __repr__(self):
-        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
-
+    manager_id = db.Column(db.Integer, nullable=True)
+    is_manager = db.Column(db.Boolean, nullable=False, default=False)
+    points = db.Column(db.Integer, default=0) 
 
 class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    # title = db.Column(db.String(100), nullable=False)
-    date_posted = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    __tablename__ = 'Posts'  
+    post_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('Users.user_id'), nullable=False)
     content = db.Column(db.Text, nullable=False)
-    author_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    emp_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    def __repr__(self):
-        return f"Post('{self.author_id}', '{self.date_posted}')"
-
-posts = [
-    {
-        'author': 'josh',
-        'emp': 'mac',
-        'points': 100,
-        'tags': ['Innovation', 'TeamWork'],
-        'content': 'lorem ipsum',
-        'date_posted': 'Aug 4, 2009'
-    },
-    {
-        'author': 'Ellie',
-        'emp': 'Christine',
-        'points': 400,
-        'tags': ['Ambition', 'TeamWork'],
-        'content': 'lorem ipsum blah blah',
-        'date_posted': 'Jan 16, 2024'
-    }
-]
-
-details = [
-    {
-        'user': 'mac',
-        'points': 1000
-    },
-    {
-        'user': 'lary',
-        'points': 800
-    },
-    {
-        'user': 'Josh',
-        'points': 600
-    }
-]
+    category = db.Column(db.String(50), nullable=False)
+    points = db.Column(db.Integer, nullable=False) 
+    timestamp = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
+    user = db.relationship('User', backref='posts')
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html', posts = posts)
+    posts = Post.query.all()
+    return render_template('home.html', posts=posts)
 
 @app.route("/new_blog")
 def new_blog():
@@ -74,6 +40,7 @@ def new_blog():
 
 @app.route("/leaderboard")
 def leaderboard():
+    details = User.query.with_entities(User.username, User.points).order_by(User.points.desc()).all()
     return render_template('leaderboard.html', title = 'leaderboard', len = len(details), details = details)
 
 if __name__ == '__main__':
