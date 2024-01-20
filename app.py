c8b824cd8f3dd11ae39a0db8d19be8fa57d093f1
khwahish2oo2 <mittalkhwahish.2108@gmail.com>
2024-01-20 14:45:26 +0530

    redeem_n_profile

--- app.py
+++ app.py
@@ -18,6 +18,7 @@ class User(db.Model):
     manager_id = db.Column(db.Integer, nullable=True)
     is_manager = db.Column(db.Boolean, nullable=False, default=False)
     points = db.Column(db.Integer, default=0) 
+    curr_points = db.Column(db.Integer, default=0) 
 
 class Post(db.Model):
     __tablename__ = 'Posts'  
@@ -29,6 +30,14 @@ class Post(db.Model):
     timestamp = db.Column(db.DateTime, default=db.func.current_timestamp(), nullable=False)
     user = db.relationship('User', backref='posts')
 
+
+##this is in order to store the coupons generated and let the company know which coupon is valid
+class coupons(db.Model):
+    __tablename__ = 'coupons'
+    coupon_id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, nullable=False)
+    coupon_name = db.Column(db.String(20), nullable=False)
+    coupon_code = db.Column(db.String(10), nullable=False)
 
 @app.route("/")
 @app.route("/home")
@@ -61,12 +70,6 @@ def login():
 
 
 
-@app.route("/register", methods=['GET', 'POST'])
-def register():
-    return render_template('register.html', title = 'Register')
-
-
-
 @app.route("/new_blog", methods=['GET', 'POST'])
 def new_blog():
     if 'user_id' not in session:
@@ -83,6 +86,7 @@ def new_blog():
         
         if employee:
             employee.points += points
+            employee.curr_points+=points
             db.session.commit()
 
         new_post = Post(user_id=employee_id, content=post_content, category=category, points=points)
@@ -112,45 +116,71 @@ def logout():
     return redirect(url_for('home'))
     
 
-# @app.route('/redeem')
-# def redeem():
-#     employee=session['user_id']
-#     employee_points = User.query.filter_by(user_id=employee).first()
-#     if employee_points:
-#         points = employee_points.points
-#         return render_template('redeem.html', points=points)
-
-
+##from here each part is required and is part of redeem
+import random
+import string
+def get_random_string(length):
+     letters = string.ascii_uppercase
+     result_str=''.join(random.choice(letters) for i in range(length))
+     return result_str
 @app.route('/redeem_points', methods=['GET', 'POST'])
 def redeem_points():
     employee = session['user_id']
     employee_points = User.query.filter_by(user_id=employee).first()
 
     if employee_points:
-        points = employee_points.points
+        points = employee_points.curr_points
+        user_id=employee_points.user_id
 
         if request.method == 'POST':
             success_messages = []
             error_messages = []
-            for option in ['zomato', 'amazon', 'boat', 'stationary', 'vacation']:
-                enabled_key = f"{option}_enabled"
-                points_key = f"{option}_points"
+            redeem_option = request.form.get('redeem_option')
+            if redeem_option:
+                points_key = f"{redeem_option}_points"
+                required_points = int(request.form.get(points_key, 0))
+                if employee_points.curr_points >= required_points:
+                    employee_points.curr_points -= required_points
+                    db.session.commit()                   
+                    success_messages.append(f'Redeemed {redeem_option.capitalize()} voucher successfully!, {required_points} points deducted.')
+                    voucher_name = f"{redeem_option.capitalize()} Voucher"
+                    voucher_worth = required_points//10
+                    
+                else:
+                    error_messages.append(f'Insufficient points to redeem {redeem_option.capitalize()}.')
 
-                if request.form.get(enabled_key) == 'on':
-                    required_points = int(request.form.get(points_key, 0))
-
-                    if employee_points.points >= required_points:
-                        employee_points.points -= required_points
-                        db.session.commit()                   
-                        success_messages.append(f'Redeemed {option.capitalize()} voucher successfully!, {required_points} points deducted.')
-                    else:
-                        error_messages.append(f'Insufficient points to redeem {option.capitalize()}.')
-                        
             if success_messages:
-                        return render_template('redeem_success.html', success_messages=success_messages, points=employee_points.points)
+                        s="F"+get_random_string(4)+str(voucher_worth)
+                        new_coupon = coupons(user_id=user_id, coupon_name=voucher_name, coupon_code=s)
+                        db.session.add(new_coupon)
+                        db.session.commit()
+                        return render_template('redeem_success.html', points=employee_points.curr_points, s=s, voucher_name=voucher_name, voucher_worth=voucher_worth)
             elif error_messages:
-                        return render_template('redeem_success.html', error_messages=error_messages, points=employee_points.points)
+                        return render_template('redeem_success.html', error_messages=error_messages, points=employee_points.curr_points)
         return render_template('redeem.html', points=points)
-   
+
+
+@app.route("/Profile", methods=['GET'])
+def Profile():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    userid = session['user_id'] 
+    employees = User.query.filter_by(user_id=userid).first()
+    employee_id = employees.user_id
+    employee_name=employees.name
+    manager=employees.manager_id
+    if manager: 
+        manager_query = User.query.filter_by(user_id=manager).first()
+        employee_manager=manager_query.name
+    employee_points = employees.points
+    employee_curr_points=employees.curr_points
+    if manager: 
+        return render_template('profile.html', title = 'New Post', id=employee_id, name=employee_name, manager=employee_manager, points=employee_points, curr_points=employee_curr_points)
+    else:
+        return render_template('profile.html', title = 'New Post', id=employee_id, name=employee_name, points=employee_points, curr_points=employee_curr_points)
 if __name__ == '__main__':
     app.run(debug=True)
+
+
+
